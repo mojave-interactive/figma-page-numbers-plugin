68d89231e7ba039ac777d5f723544108ae8d0db4
Greg Whitescarver <gregory.whitescarver@mojave.agency>
2023-11-30 15:31:50 -0800

    Automatically inserts numbers 1..n into layers named "page number". Frames are traversed left to right, top to bottom.

--- code.ts
+++ code.ts
@@ -1,53 +1,72 @@
-class ContainerFrame {
-  constructor(
-    public x: number,
-    public y: number,
-    public frameNode: FrameNode
-  ) {}
-}
-figma.skipInvisibleInstanceChildren = true
-const nodes = figma.currentPage.selection
-var frameMatrix:ContainerFrame[] = []
-figma.notify(`Found ${nodes.length} selected nodes.`)
-for (var node of nodes) {
-  console.log(node.type)
-  if (node.type === 'FRAME' && node.visible && node.children.length > 0) {
-    frameMatrix.push({ y: node.y, x: node.x, frameNode: node })
+class PageNumberer {
+  frameMatrix: { x: number, y: number, frameNode: FrameNode }[] = []
+  layersToNumber: { layer: TextNode, number: number }[] = []
+
+  constructor() { 
+    this.buildFrameMatrix()
   }
-}
-for (var frame of frameMatrix) {
-  var pageNumberLayers:TextNode[] = []
-  //@ts-ignore - we're appropriately checking for the type of node
-  pageNumberLayers = frame.frameNode.findAll(n => n.name === "page number" && n.type === "TEXT")
-  if (pageNumberLayers.length > 0) {
-    for (var layer of pageNumberLayers) {
-      debugger
-      layer.characters = "99"
+
+  public buildFrameMatrix() {
+    const selectedNodes = figma.currentPage.selection
+    figma.notify(`Found ${selectedNodes.length} selected nodes.`)
+    for (var node of selectedNodes) {
+      if (node.type === 'FRAME' && node.visible && node.children.length > 0) {
+        this.frameMatrix.push({ y: node.y, x: node.x, frameNode: node })
+      }
     }
+    this.frameMatrix.sort((a, b) => {
+      if (a.y === b.y) {
+        return a.x - b.x
+      } else {
+        return a.y - b.y
+      }
+    })
   }
-}
-debugger
-//console.log(frameMatrix)
-figma.closePlugin();
 
-/* // This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+  public updatePageNumbersAndFinish() {
+    var pageNumber = 1
+    var fontsToLoad: FontName[] = []
+    for (var frame of this.frameMatrix) {
+      var pageNumberLayers:TextNode[] = []
+      //@ts-ignore - we're appropriately checking for the type of node
+      pageNumberLayers = frame.frameNode.findAll(n => n.name === "page number" && n.type === "TEXT")
+      if (pageNumberLayers.length > 0) {
+        for (var layer of pageNumberLayers) {
+          this.layersToNumber.push({ layer: layer, number: pageNumber })
+          this.checkForFontsToLoad(layer, fontsToLoad)
+        }
+      }
+      //increment even if the frame doesn't have a page number layer
+      pageNumber++
+    }
+    // get an array of loadFontAsync() promises, one for each entry in foundFonts[]
+    const fontPromises = fontsToLoad.map(f => figma.loadFontAsync(f));
+    // text is set only after all fonts are loaded
+    Promise.all(fontPromises).then(() => {
+        this.layersToNumber.map(l => this.setTextOfNode(l.layer, l.number.toString()))
+        figma.closePlugin();
+    })
+  }
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+  checkForFontsToLoad = (layer: TextNode, fontsToLoad: FontName[]) => {
+    if (layer.hasMissingFont) {
+      throw  "One or more fonts is missing.";
+    }
+    for (let i = 0; i < layer.characters.length; i++) {
+      const fontName = layer.getRangeFontName(i, i + 1) as FontName;
+      if (fontsToLoad.find(f => f.family === fontName.family) === undefined) {
+        fontsToLoad.push(fontName);
+      }
+    }
+    return fontsToLoad;
+  }
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+  setTextOfNode = (textNode: TextNode, text: string) => {
+    textNode.characters = text;
+    return textNode;
+  }
+}    
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen. */
+figma.skipInvisibleInstanceChildren = true
+const pageNumberer = new PageNumberer()
+pageNumberer.updatePageNumbersAndFinish()
