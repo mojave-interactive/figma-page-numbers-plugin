f074297106a27636629b7f750ad92978a1ba7495
Gregory Whitescarver <gregory.whitescarver@mojave.agency>
2024-02-29 09:25:44 -0800

    Added GPL 3.0 license

--- code.ts
+++ code.ts
@@ -1,3 +1,21 @@
+/*
+ * Page Numbers Figma Plugin
+ *
+ * Copyright (c) 2023 Mojave Interactive LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For a copy of the GNU General Public License version 3.0,
+ * please visit: https://www.gnu.org/licenses/gpl-3.0.html
+ */
 class PageNumberer {
   selectedNodeMatrix: { x: number, y: number, selectedNode: BaseNode }[] = []
   ignoreNonFrameNodes: boolean
